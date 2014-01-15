ff63765d9562a34681347bf5a3adcaf2167daf31
Oliver Mihatsch <oliver.mihatsch@panoratio.com>
2014-01-15 14:41:09 +0100

    Special pano version

--- WeceemGrailsPlugin.groovy
+++ WeceemGrailsPlugin.groovy
@@ -8,7 +8,7 @@ class WeceemGrailsPlugin {
     def _log = LogFactory.getLog('org.weceem.WeceemGrailsPlugin')
 
     // the plugin version
-    def version = "1.2-SNAPSHOT"
+    def version = "1.2-grails_2_2_1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
 
